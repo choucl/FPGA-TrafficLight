ff8d18e28c30026eb3e90f71cc46d659ba29eda9
choucl <clc25600@gmail.com>
2022-03-10 10:34:47 +0800

    Add define file for problem1

--- Problem1/src/def.v
+++ Problem1/src/def.v
@@ -12,9 +12,3 @@
 `define RED 2'd1
 `define GREEN 2'd2
 `define YELLOW 2'd3
-
-/* controller size define */
-`define SW_SZ 2
-`define BTN_SZ 3
-`define LED_SZ 3    // len for RGB LED
-`define TIME_SZ 4   // len for time
