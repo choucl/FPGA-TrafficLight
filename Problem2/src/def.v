d94be9f526a952f41177a64aa36a628ec007e017
choucl <clc25600@gmail.com>
2022-03-10 10:31:16 +0800

    Add define file

---
+++ Problem2/src/def.v
@@ -0,0 +1,16 @@
+/* position that controls color of LED */
+`define B_POS 2
+`define G_POS 1
+`define R_POS 0
+
+/* used colors */
+`define WHITE 2'd0
+`define RED 2'd1
+`define GREEN 2'd2
+`define YELLOW 2'd3
+
+/* controller size define */
+`define SW_SZ 2
+`define BTN_SZ 3
+`define LED_SZ 3    // len for RGB LED
+`define TIME_SZ 4   // len for time
