2f72ac53e5bf60a0e05bf4712ebc14956f08b2c1
choucl <clc25600@gmail.com>
2022-03-11 09:42:49 +0800

    Add rst_i signal to controller

--- Problem2/src/Controller.v
+++ Problem2/src/Controller.v
@@ -6,6 +6,7 @@
 
 module Controller (
     input                     clk_i,
+    input                     rst_i,
     input                     time_i,
     input      [`SW_SZ-1:0]   sw_i, 
     input      [`BTN_SZ-1:0]  btn_i, 
@@ -28,65 +29,77 @@ module Controller (
     reg [`TIME_SZ-1:0] yrlen = `DEFAULT_YR;
     reg [`TIME_SZ-1:0] rrlen = `DEFAULT_RR;
     
-    always @(posedge time_i) begin
-        if (ctime != 0) begin          // time != 0, count down
-            ctime <= ctime - 1;
-        end else begin                 // time == 0, change state
-            // state transition
-            if (current == RR_2) begin
-                current <= GR;
-            end else begin
-                current <= current + 3'd1;
-            end
-            // change count down time for new state
-            if (current == GR || current == RG) begin
-                ctime <= grlen;
-            end else if (current == YR || current == RY) begin
-                ctime <= yrlen;
-            end else begin
-                ctime <= rrlen;
+    always @(posedge time_i or posedge rst_i) begin
+        if (rst_i) begin 
+            current <= GR;
+            ctime   <= `DEFAULT_GR;
+        end else begin
+            if (ctime != 0) begin          // time != 0, count down
+                ctime <= ctime - 1;
+            end else begin                 // time == 0, change state
+                // state transition
+                if (current == RR_2) begin
+                    current <= GR;
+                end else begin
+                    current <= current + 3'd1;
+                end
+                // change count down time for new state
+                if (current == GR || current == RG) begin
+                    ctime <= grlen;
+                end else if (current == YR || current == RY) begin
+                    ctime <= yrlen;
+                end else begin
+                    ctime <= rrlen;
+                end
             end
         end
     end
 
     // switch/btn control
     reg [15:0] db_cnt;  // debounce counter
-    always @(posedge clk_i) begin
-        if (db_cnt == 16'hffff) begin
-            if (btn_i[0] == 1'b1) begin            // reset 
-                db_cnt <= 0;
-                case (sw_i)
-                    `ADJ_GR: grlen <= `DEFAULT_GR;
-                    `ADJ_YR: yrlen <= `DEFAULT_YR;
-                    `ADJ_RR: rrlen <= `DEFAULT_RR;
-                    `NORMAL: begin end
-                endcase
-            end else if (btn_i[1] == 1'b1) begin  // add 1 sec
-                db_cnt <= 0;
-                case (sw_i)
-                    `ADJ_GR: grlen <= (grlen == 15)? grlen : grlen + 1;
-                    `ADJ_YR: yrlen <= (yrlen == 15)? yrlen : yrlen + 1;
-                    `ADJ_RR: rrlen <= (rrlen == 15)? rrlen : rrlen + 1;
-                    `NORMAL: begin end
-                endcase
-            end else if (btn_i[2] == 1'b1) begin  // minus 1 sec
-                db_cnt <= 0;
-                case (sw_i)
-                    `ADJ_GR: grlen <= (grlen == 0)? grlen : grlen - 1;
-                    `ADJ_YR: yrlen <= (yrlen == 0)? yrlen : yrlen - 1;
-                    `ADJ_RR: rrlen <= (rrlen == 0)? rrlen : rrlen - 1;
-                    `NORMAL: begin end
-                endcase
-            end
+    always @(posedge clk_i or posedge rst_i) begin
+        if (rst_i) begin
+            db_cnt <= 16'hffff;
+            grlen  <= `DEFAULT_GR;
+            yrlen  <= `DEFAULT_YR;
+            rrlen  <= `DEFAULT_RR;
         end else begin
-            db_cnt <= db_cnt + 1;
-            case (sw_i)
-                `ADJ_GR: led_o <= grlen;
-                `ADJ_YR: led_o <= yrlen;
-                `ADJ_RR: led_o <= rrlen;
-                `NORMAL: led_o <= ctime;
-            endcase
+            if (db_cnt == 16'hffff) begin
+                if (btn_i[0] == 1'b1) begin            // reset 
+                    db_cnt <= 0;
+                    case (sw_i)
+                        `ADJ_GR: grlen <= `DEFAULT_GR;
+                        `ADJ_YR: yrlen <= `DEFAULT_YR;
+                        `ADJ_RR: rrlen <= `DEFAULT_RR;
+                        `NORMAL: begin end
+                    endcase
+                end else if (btn_i[1] == 1'b1) begin  // add 1 sec
+                    db_cnt <= 0;
+                    case (sw_i)
+                        `ADJ_GR: grlen <= (grlen == 15)? grlen : grlen + 1;
+                        `ADJ_YR: yrlen <= (yrlen == 15)? yrlen : yrlen + 1;
+                        `ADJ_RR: rrlen <= (rrlen == 15)? rrlen : rrlen + 1;
+                        `NORMAL: begin end
+                    endcase
+                end else if (btn_i[2] == 1'b1) begin  // minus 1 sec
+                    db_cnt <= 0;
+                    case (sw_i)
+                        `ADJ_GR: grlen <= (grlen == 0)? grlen : grlen - 1;
+                        `ADJ_YR: yrlen <= (yrlen == 0)? yrlen : yrlen - 1;
+                        `ADJ_RR: rrlen <= (rrlen == 0)? rrlen : rrlen - 1;
+                        `NORMAL: begin end
+                    endcase
+                end
+            end else begin
+                db_cnt <= db_cnt + 1;
+            end
         end
+        case (sw_i)
+            `ADJ_GR: led_o <= grlen;
+            `ADJ_YR: led_o <= yrlen;
+            `ADJ_RR: led_o <= rrlen;
+            `NORMAL: led_o <= ctime;
+        endcase
     end
     
     // change LED color according to different states
