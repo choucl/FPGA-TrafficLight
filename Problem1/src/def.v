29eec261a23cc3a013199ab0cbf8bface26107cd
Pong En-Yu <andrew0911114933@gmail.com>
2022-03-11 23:06:47 +0800

    Delete unused defined value

--- Problem1/src/def.v
+++ Problem1/src/def.v
@@ -2,12 +2,6 @@
  * Define some neccessary values in this project
  */
 
-/* position that controls color of LED */
-`define B_POS 2
-`define G_POS 1
-`define R_POS 0
-
-/* used colors */
 `define WHITE 2'd0
 `define RED 2'd1
 `define GREEN 2'd2
