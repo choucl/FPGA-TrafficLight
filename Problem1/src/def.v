e8779d09246a5d74518ab99f0784a25c04940956
Pong En-Yu <andrew0911114933@gmail.com>
2022-03-11 23:05:10 +0800

    Add color codes

--- Problem1/src/def.v
+++ Problem1/src/def.v
@@ -12,3 +12,9 @@
 `define RED 2'd1
 `define GREEN 2'd2
 `define YELLOW 2'd3
+
+/* color codes */
+`define WHITE_CODE  3'b111
+`define RED_CODE    3'b001
+`define GREEN_CODE  3'b010
+`define YELLOW_CODE 3'b011
